0388d1f9e576d250a766ca87dec8e742292c2ddd
Fatou Samb <sambfatou210@gmail.com>
2026-05-13 10:22:31 +0000

    A createCart() factory function that returns an object with four           methods: addItem(item), removeItem(id), getTotal(), and getSummary().

--- cart-manager.js
+++ cart-manager.js
@@ -1,66 +1,30 @@
 function createCart() {
-    let listItem = [];
-    let itemCounter = 0;
+    let listItems = [];
+    let nextId = 1;
 
-    function addItem(name, price, quantity) {
-        if (price < 0 || quantity < 0) {
-            throw new Error("The price and the quatity cannot be negative");
-        } else {
-            itemCounter ++;
-            const newItem = {
-                id: itemCounter,
-                name: name,
-                price: price,
-                quantity: quantity,
-            };
-            listItem.push(newItem);
-        }
+    function addItem(item) {
+      const newItem = { id: nextId++, ...item };
+      listItems.push(newItem);
     }
-
+  
     function removeItem(id) {
-        let found = false;
-        for (let i = 0; i < listItem.length; i++) {
-             if(id == listItem[i].id) {
-                found = true;
-                listItem.splice((i), 1);
-            }   
-        }
-        if (found == false) {
-            throw new Error("Item not found!");
-        }
-        // Or we can do:
-        // listItem = listItem.filter(item => item.id !== id);
+      listItems = listItems.filter(item => item.id !== id);
     }
 
     function getTotal() {
-        let total = 0;
-        for (let i = 0; i < listItem.length; i++) {
-            total += listItem[i].price * listItem[i].quantity;
-        }
-        return total;
-        // Or we can do:
-        // return listItem.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+      return listItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
     }
 
     function getSummary() {
-        listItem.forEach((item) => {
-            console.log(`Id: ${item.id}, Name: ${item.name}, Price: ${item.price}, Quantity: ${item.quantity}, Line Total: ${item.price * item.quantity}`);
-        });
-        console.log(`Total: ${getTotal()}`);
-        
+      listItems.forEach(item => {
+        console.log(`Name: ${item.name} | Quantity: ${item.quantity} | Total: ${item.quantity * item.price}`);
+      });
+
+      console.log(`Cart's grand total: ${getTotal()}`);
+      
     }
 
-    return {addItem, removeItem, getTotal, getSummary};
-
+    return { addItem, removeItem, getTotal, getSummary }
 }
 
-Cosmetic = createCart();
-Cosmetic.addItem("Crème", 4000, 4);
-Cosmetic.addItem("Savon Mayanna", 1500, 15);
-Cosmetic.addItem("Déodorant Dove", 2000, 20);
-Cosmetic.addItem("Nova 432", 4500, 15);
 
-console.log(Cosmetic.getTotal());
-Cosmetic.getSummary();
-Cosmetic.removeItem(3);
-Cosmetic.getSummary();
